c5070dbe52f1c7b7cfa7a95c47bd3d9fb7e7f3c5
jon jeous <qjjjrecto@tip.edu.ph>
2025-04-05 18:01:45 +0800

    commit

--- ERC721/contracts/myNFT.sol
+++ ERC721/contracts/myNFT.sol
@@ -1,15 +1,24 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.20;
-import  "@penzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+
 
 contract Boruto is ERC721URIStorage {
+    uint256 tokenId;
 
-	uint256: tokenId;
 
-	constructor() ERC721 ("Boruto", "BRT){}
+    constructor() ERC721 ("Boruto", "BRT"){}
 
-	function mint() external {
-		_safeMint(msg.sender, tokenId);
-		// "tokenURI"
+
+    function mint() external {
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(
+            tokenId,
+            ""
+            
+        );
+        tokenId++;
+    }
+}
